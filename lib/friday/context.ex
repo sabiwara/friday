6c3e859d02f9313b607b55a128f6acf7d18dda7d
sabiwara <sabiwara@gmail.com>
2022-02-26 22:08:31 +0900

    Make now a param

--- lib/friday/context.ex
+++ lib/friday/context.ex
@@ -2,11 +2,28 @@ defmodule Friday.Context do
   @type country :: :jp | :fr
   @friday 5
 
-  @spec describe_today(country()) :: String.t()
-  def describe_today(country) do
+  @doc """
+
+  ## Examples
+
+      iex> describe_today(:jp, ~U[2022-02-24 12:01:01Z])
+      "金曜日じゃない :("
+
+      iex> describe_today(:jp, ~U[2022-02-24 16:01:01Z])
+      "金曜じゃ！"
+
+      iex> describe_today(:fr, ~U[2022-02-24 16:01:01Z])
+      "Ce n'est pas vendredi :("
+
+      iex> describe_today(:fr, ~U[2022-02-24 23:01:01Z])
+      "C'est vendredi !"
+
+  """
+  @spec describe_today(country(), DateTime.t()) :: String.t()
+  def describe_today(country, utc_now \\ DateTime.utc_now()) do
     timezone = get_timezone(country)
 
-    DateTime.utc_now()
+    utc_now
     |> DateTime.shift_zone!(timezone)
     |> DateTime.to_date()
     |> Date.day_of_week()
