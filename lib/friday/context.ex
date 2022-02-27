53019d6409272b9d17144bed55318cdd03510b4f
sabiwara <sabiwara@gmail.com>
2022-02-27 09:18:25 +0900

    Use constant

--- lib/friday/context.ex
+++ lib/friday/context.ex
@@ -1,5 +1,6 @@
 defmodule Friday.Context do
   @type country :: :jp | :fr
+  @friday 5
 
   @spec describe_today(country()) :: String.t()
   def describe_today(country) do
@@ -15,10 +16,10 @@ defmodule Friday.Context do
   defp get_timezone(:jp), do: "Asia/Tokyo"
   defp get_timezone(:fr), do: "Europe/Paris"
 
-  defp get_message(5, :jp), do: "金曜じゃ！"
+  defp get_message(@friday, :jp), do: "金曜じゃ！"
   defp get_message(_day, :jp), do: "金曜日じゃない :("
 
-  defp get_message(5, :fr), do: "C'est vendredi !"
+  defp get_message(@friday, :fr), do: "C'est vendredi !"
   defp get_message(_day, :fr), do: "Ce n'est pas vendredi :("
 
   def validate_country("jp"), do: {:ok, :jp}
